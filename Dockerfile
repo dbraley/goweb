44b83834892d40acb48b64df57b078f7a299f595
David Braley <david.braley@gmail.com>
2018-05-31 09:10:24 -0400

    Add Dockerfiles

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.10.0-alpine3.7 as builder
+RUN apk add --update --no-cache alpine-sdk ca-certificates \
+      libressl \
+      git openssh openssl build-base coreutils upx
+WORKDIR /go/src/github.com/dbraley/goweb
+RUN go get -d -v github.com/gorilla/mux
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -ldflags '-w -s' -o main main.go
+#RUN upx --brute main
+
+FROM scratch
+COPY --from=builder /go/src/github.com/dbraley/goweb/main /
+EXPOSE 8080
+CMD ["/main"]
--- main.go
+++ main.go
@@ -20,7 +20,7 @@ type Address struct {
 
 var people []Person
 
-func main()  {
+func main() {
 	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
 	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
 	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
@@ -30,7 +30,7 @@ func main()  {
 	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
 	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
 	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func GetPeople(w http.ResponseWriter, r *http.Request) {
